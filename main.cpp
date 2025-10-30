f66536ac68f08cb23afa1113aca47411d4bffd6b
Tomcup <jiligulu7755@outlook.com>
2025-10-30 13:20:08 +0800

    7-5 找鞍点 未完成

--- main.cpp
+++ main.cpp
@@ -1,45 +1,62 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
-#include <string>
 
 int main()
 {
-    int T;
-    std::cin >> T;
-    while (T)
+    int n;
+    std::cin >> n;
+
+    if (n == 1)
     {
-        --T;
-
-        int n;
-        std::cin >> n;
-
-        std::vector<std::vector<int>> num;
-        num.resize(n);
-
-        for (int i{0}; i < n; ++i)
-        {
-            num.at(i).resize(n);
-            for (int j{0}; j < n; ++j)
-            {
-                std::cin >> num.at(i).at(j);
-            }
-        }
-
-        bool hey{};
-
-        for (int i{0}; i < n; ++i)
-        {
-            for (int j{0}; j < n - i - 1; ++j)
-            {
-                if (num.at(n - 1 - i).at(j) != 0)
-                    hey = true;
-            }
-        }
-
-        if (hey)
-            std::cout << "NO" << std::endl;
-        else
-            std::cout << "YES" << std::endl;
+        std::cout << "NONE" << std::endl;
+        return 0;
     }
+
+    std::vector<std::vector<int>> num;
+    num.resize(n);
+
+    for (int i{0}; i < n; ++i)
+    {
+        num.at(i).resize(n);
+        for (int j{0}; j < n; ++j)
+        {
+            std::cin >> num.at(i).at(j);
+        }
+    }
+
+    bool s{true};
+    for (int i{0}; i < n; ++i) // 行
+    {
+        for (int j{0}; j < n; ++j) // 列
+        {
+            s = true;
+            for (int k{0}; k < n; ++k) // 行最大
+            {
+                if (k == j)
+                    continue;
+                if (num.at(i).at(k) >= num.at(i).at(j))
+                    s = false;
+            }
+
+            if (s)
+            {
+                for (int k{0}; k < n; ++k) // 列最大
+                {
+                    if (k == i)
+                        continue;
+                    if (num.at(k).at(j) <= num.at(i).at(j))
+                        s = false;
+                }
+                if (s)
+                {
+                    std::cout << i << ' ' << j << std::endl;
+                    return 0;
+                }
+            }
+        }
+    }
+
+    std::cout << "NONE" << std::endl;
+    return 0;
 }
